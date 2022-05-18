390745ecb0046d2ce7a3f975715510380fc8e2cd
nayloe28 <100226971+nayloe28@users.noreply.github.com>
2022-05-18 15:31:30 +0700

    Update main.c

--- main.c
+++ main.c
@@ -4,52 +4,72 @@
 
 int main(int banyakArgumen, char *argumen[]) //./main username password
 {   
-    FILE *fpw = fopen("database/login.bin", "wb");
-    char login[20] = "Sayfull@Code";
-
-    fwrite(login, sizeof(char), sizeof(login)/sizeof(char), fpw);
-    fclose(fpw);
-
-    if(banyakArgumen != 3){
-        printf("Gagal login!");
-        printf("\nCara Penggunaan : ./FileAplikasiProgramUtama username password");
+    char login[50];
+    
+    if(banyakArgumen == 1)
+    {
+        FILE *fpw = fopen("database/login.bin", "wb");
+        printf("daftar akun dengan format username@password : ");
+        scanf("%s", & *login);
+        fwrite(login, sizeof(char), sizeof(login)/sizeof(char), fpw);
+        fclose(fpw);
+        printf("Jika ingin melanjutkan proses login, ketik\n./FileAplikasiProgramUtama username password\n");
     }
 
+    else if(banyakArgumen != 3)
+    {
+        printf("Input yang anda masukkan tidak sesuai format");
+        exit(0);    
+    }
+    
+    while(banyakArgumen==3)
+    {
+        break;
+    }
+    
     char usernameInput[10], passwordInput[10];
     strcpy(usernameInput, argumen[1]);
     strcpy(passwordInput, argumen[2]);
 
     FILE *fpr;
 
-    if((fpr = fopen("database/login.bin", "rb")) == NULL){
+    if((fpr = fopen("database/login.bin", "rb")) == NULL)
+    {
         printf("Gagal membuka file!");
          return EXIT_FAILURE;
     }
 
     char akun[20];
-fread(akun, sizeof(char), sizeof(akun)/sizeof(char), fpr);
+    
+    fread(akun, sizeof(char), sizeof(akun)/sizeof(char), fpr);
 
-fclose(fpr);
+    fclose(fpr);
 
-char *string[3];
-char username[20], password[20];
-int ctrl = 0;
+    char *string[3];
+    char username[20], password[20];
+    int ctrl = 0;
 
-string[0] = strtok(akun, "@");
-while(string[ctrl++] != NULL)
-{
-    string[ctrl] = strtok(NULL, "@");
-}
+    string[0] = strtok(akun, "@");
+    
+    while(string[ctrl++] != NULL)
+    {
+        string[ctrl] = strtok(NULL, "@");
+    }
+    
+    while(string[ctrl++] == NULL)
+    {
+     printf("anda belum membuat akun\njika ingin registrasi ketik dengan format : ./fileaplikasiprogramutama");
+    }
+    
+    strcpy(username, string[0]);
+    strcpy(password, string[1]);
 
-strcpy(username, string[0]);
-strcpy(password, string[1]);
-
-if( (strcmp(usernameInput, username) == 0) && (strcmp(passwordInput, password) == 0))
-{
-    printf("Selamat, Anda berhasil login !");
-}
-else {
-    printf("Anda gagal login!");
-}
-
+    if( (strcmp(usernameInput, username) == 0) && (strcmp(passwordInput, password) == 0))
+    {
+        printf("Selamat, Anda berhasil login !");
+    }
+    else 
+    {
+        printf("Password atau username yang anda masukkan salah");
+    }
 }
